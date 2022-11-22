13569421193a5616795a1ce3fc496383f2859e1c
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2022-11-22 11:35:05 -0800

    add code to force ignore ssl verification

--- src/main/java/gov/nasa/pds/registry/common/es/client/HttpConnectionFactory.java
+++ src/main/java/gov/nasa/pds/registry/common/es/client/HttpConnectionFactory.java
@@ -2,12 +2,20 @@ package gov.nasa.pds.registry.common.es.client;
 
 
 import java.net.HttpURLConnection;
+
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
+
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
 
 import org.apache.http.HttpHost;
 
@@ -48,9 +56,39 @@ public class HttpConnectionFactory
      */
     public HttpURLConnection createConnection() throws Exception
     {
+    	// Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+ 
+    	
+    	 // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+ 
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+ 
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+    	
         HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        
         con.setConnectTimeout(timeout);
         con.setReadTimeout(timeout);
+
         con.setAllowUserInteraction(false);
         
         if(authHeader != null)
