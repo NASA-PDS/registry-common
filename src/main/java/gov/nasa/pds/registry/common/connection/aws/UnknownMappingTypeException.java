0060c66f2df1e85e1923292d4ba02845f3b783d2
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2025-11-17 09:54:33 -0800

    add new Exception for unknown opensearch types

---
+++ src/main/java/gov/nasa/pds/registry/common/connection/aws/UnknownMappingTypeException.java
@@ -0,0 +1,11 @@
+package gov.nasa.pds.registry.common.connection.aws;
+
+public class UnknownMappingTypeException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  public UnknownMappingTypeException(String s) {
+    super(s);
+  }
+
+}
